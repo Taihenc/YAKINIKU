220a8236a5dd8a78acf220d2792f0f82d02c8e3f
Taihenc <51960471+Taihenc@users.noreply.github.com>
2024-01-08 00:16:56 +0700

    ✅home

--- public/components/home.js
+++ public/components/home.js
@@ -27,14 +27,14 @@ export default function home(page_id, is_show) {
                 <button id='start-now-button' class='card-button text-para-light'>START NOW</button>
             </div>
             <div id='svg-grill'>
-                <img src="../img/01_home/pan.png" alt="" />
+                <img src="./img/01_home/pan.png" alt="" />
             </div>
             <button id='start-now-button' class='card-button card-button-mobile text-para-light'>START NOW</button>
             <div class='card-right'>
                 <div class='card-header text-header'>WHY BEEF?</div>
                 <div class='card-sub-header text-sub-header'>THE KING OF ALL MEAT.</div>
                 <div class='card-cow-img'>
-                    <img src="../img/01_home/cowicon.png" alt="">
+                    <img src="./img/01_home/cowicon.png" alt="">
                 </div>
                 <div class='card-para-text text-para-light'>
                     Beef's rich history spans centuries. Its enduring popularity is rooted in its flavor, nutrition,
--- server.js
+++ server.js
@@ -1,49 +1,17 @@
-import express from 'express';
 import home from './public/components/home.js';
-import breed from './public/components/breed.js';
-import cut from './public/components/cut.js';
-import cooking from './public/components/cooking.js';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+// import breed from './public/components/breed.js';
+// import cut from './public/components/cut.js';
+// import cooking from './public/components/cooking.js';
+// import fs from 'fs';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
 
-const app = express();
-const PORT = 80;
-// const backend_PORT = 8080;
-
-app.use(express.static('public'));
-
-app.listen(PORT, '0.0.0.0', () => {
-	console.log(`Frontend Server ready at http://localhost:${PORT}`);
-});
-
-app.get('/', (req, res) => {
-	// Set_backend_url(`${req.protocol}://${req.get('host')}:${backend_PORT}`);
-	fs.readFile(
-		path.resolve('./public/template.html'),
-		'utf-8',
-		(err, data) => {
-			if (err) {
-				console.log(err);
-				res.status(500).send('Some error happened');
-			}
-			let html = data;
-			let append = '';
-			(async () => {
-				append += home(0, true);
-				append += await breed(1, false);
-				append += await cut(2, false);
-				append += await cooking(3, false);
-				return res.send(html.replace('<!-- replace me! -->', append));
-			})();
-		}
-	);
-});
-
-// function Set_backend_url(url) {
-// 	const config_path = `${fileURLToPath(import.meta.url)}`;
-// 	const regex = /\b(backend_url\s*=\s*')[^']*'/i;
-
-// 	let data = fs.readFileSync(config_path, 'utf-8');
-// 	fs.writeFileSync(config_path, data.replace(regex, `$1${url}'`), 'utf-8');
-// }
+// let html = data;
+let append = '';
+(async () => {
+	append += home(0, true);
+	// append += await breed(1, false);
+	// append += await cut(2, false);
+	// append += await cooking(3, false);
+})();
+document.querySelector('#replace-me').innerHTML = append;
